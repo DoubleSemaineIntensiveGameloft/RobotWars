729c9dd3d923139d45bfeffb957035c5c1e9a4b6
itsgama <itsgama@gmail.com>
2015-03-10 18:00:07 +0100

    Revert "Commit First proto Combat"
    This reverts commit 7d80bf17be381d02e3218965d9410c27f2137044.

--- Assets/Dpad.cs
+++ Assets/Dpad.cs
@@ -3,53 +3,12 @@ using System.Collections;
 
 public class Dpad : MonoBehaviour {
 
-    public bool active = false;
-
-    public Transform pad;
-
-    public Bot bot;
-
-    public float distanceMax = 5;
-
-    
 
 	void Start () {
-
-        pad = transform.FindChild("Pad");
 	
 	}
 
 	void Update () {
-
-        if(!active)
-        { 
-            pad.transform.localPosition = Vector3.zero;
-        } else
-        {
-            Vector3 padDirection = (pad.transform.position - transform.position).normalized;
-            bot.Move(new Vector3(padDirection.x, 0, padDirection.y));
-        }
 	
 	}
-
-    public void UpdatePad()
-    {
-        active = true;
-        pad.transform.position = Input.mousePosition;
-
-        Vector3 padDirection = (pad.transform.position - transform.position).normalized;
-
-        if(Vector3.Distance(pad.position, transform.position) > distanceMax)
-        {
-            pad.transform.localPosition = padDirection * distanceMax;
-        }
-
-
-
-    }
-
-    public void DeActivate()
-    {
-        active = false;
-    }
 }
