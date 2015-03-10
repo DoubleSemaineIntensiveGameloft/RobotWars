34e671a7fa332af9f9a83589976c7c10fccdbcd9
itsgama <itsgama@gmail.com>
2015-03-10 18:00:52 +0100

    Revert "Revert "Commit First proto Combat""
    This reverts commit 729c9dd3d923139d45bfeffb957035c5c1e9a4b6.

---
+++ Assets/CameraGame.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class CameraGame : MonoBehaviour {
+
+    [Range(-1,1)]
+    public float cameraGyro = 0;
+
+    public float actCameraGyro = 0;
+
+
+    public Vector3 topPosition;
+    public Vector3 sidePosition;
+
+    public bool debugPC = false;
+
+	void Start () {
+
+	}
+	
+	void Update () {
+        
+
+        if(!debugPC)
+        { 
+            cameraGyro = Mathf.Round(Input.acceleration.y * -1);
+        }
+
+
+        actCameraGyro = Mathf.Lerp(actCameraGyro, cameraGyro, Time.deltaTime * 2);
+
+        transform.position = Vector3.Lerp(topPosition, sidePosition, actCameraGyro);
+
+
+
+        transform.LookAt(Vector3.zero);
+	}
+
+    public bool gyoBool { get; set; }
+
+    public Gyroscope gyro { get; set; }
+}
