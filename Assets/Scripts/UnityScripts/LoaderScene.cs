433c2b38407294bb47fd9c6691bb7489ab29dea9
itsgama <itsgama@gmail.com>
2015-03-19 11:41:25 +0100

    Fix

---
+++ Assets/Scripts/UnityScripts/LoaderScene.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoaderScene : MonoBehaviour {
+
+    public string levelName;
+
+	// Use this for initialization
+	void Start () {
+        Application.LoadLevel(levelName);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
